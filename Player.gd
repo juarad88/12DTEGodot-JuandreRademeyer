736d7eff38c106208523890a6efd961ca7fdd0a2
Juandré Rademeyer <juarad88@kingshigh.school.nz>
2023-07-18 11:00:37 +1200

    improved environment to make my game as it should be

--- Player.gd
+++ Player.gd
@@ -10,7 +10,7 @@ var pages_collected = 0
 @onready var ray = $Camera3D/RayCast3D
 @onready var interaction_notifier = $Control/InteractionNotifier
 @onready var collection_tracker = $Control/MarginContainer/CollectionTracker
-#@onready var torch = $Camera3D/Node3D
+@onready var torch = $Camera3D/Node3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 20
@@ -37,8 +37,8 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-#	if Input.is_action_just_pressed("torch"):
-#		torch.visible = not torch.visible
+	if Input.is_action_just_pressed("torch"):
+		torch.visible = not torch.visible
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
